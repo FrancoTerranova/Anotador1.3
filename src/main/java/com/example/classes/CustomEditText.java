dfe31d9205a888d049228480d0666b46f59b82d4
FrancoTerranova <37856276+FrancoTerranova@users.noreply.github.com>
2020-10-20 02:08:56 -0300

    cust edit text

---
+++ src/main/java/com/example/classes/CustomEditText.java
@@ -0,0 +1,42 @@
+package com.example.classes;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.adapters.TextViewBindingAdapter;
+
+public class CustomEditText extends androidx.appcompat.widget.AppCompatEditText {
+
+
+    public CustomEditText(@NonNull Context context) {
+        super(context);
+    }
+
+    public CustomEditText(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomEditText(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void onSelectionChanged(int start, int end) {
+
+        CharSequence text = getText();
+        if (text != null) {
+            if (start != text.length() || end != text.length()) {
+                setSelection(text.length(), text.length());
+                return;
+            }
+
+
+        }
+
+        super.onSelectionChanged(start, end);
+    }
+
+}
